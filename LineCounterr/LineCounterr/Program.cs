694941e184d70113819b4974c84a0f1c24c89c74
Vitkir <kirillcherevko@gmail.com>
2019-08-08 17:40:19 +0400

    code refactoring

--- LineCounterr/LineCounterr/Program.cs
+++ LineCounterr/LineCounterr/Program.cs
@@ -10,22 +10,31 @@ namespace LineCounterr
     {
         static void Main(string[] args)
         {
-            int i;
+            int rowCount = ReadPositiveValue();
+            Draw(rowCount);
+            Console.ReadKey();
+        }
+
+        private static void Draw(int rowCount)
+        {
+            for (int i = 1; i <= rowCount; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+
+        private static int ReadPositiveValue()
+        {
+            int value;
             do
             {
-                bool Lines = int.TryParse(Console.ReadLine(), out i);
-                if (!Lines)
+                if (!int.TryParse(Console.ReadLine(), out value))
                 {
                     Console.WriteLine("Enter num");
-
                 }
             }
-            while (i <= 0);
-            for (int x = 1; x <= i; x++)
-            {
-                Console.WriteLine(new string('*', x));
-            }
-            Console.ReadKey();
+            while (value <= 0);
+            return value;
         }
     }
 }
