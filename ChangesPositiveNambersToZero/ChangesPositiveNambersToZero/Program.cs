c1a434cd572fd0ad0d3c9e493ab4c7bd2c8465e2
Vitkir <kirillcherevko@gmail.com>
2019-08-10 13:00:03 +0400

    Task 1.8

---
+++ ChangesPositiveNambersToZero/ChangesPositiveNambersToZero/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangesPositiveNambersToZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] array = new int[5, 5, 5];
+        }
+    }
+}
