799e4d47d7d7d7906867995149a2fc87daf29e91
Vitkir <kirillcherevko@gmail.com>
2019-08-14 00:40:39 +0400

    Task 1.9 for verification.

---
+++ SumNotNegativeValue/SumNotNegativeValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumNotNegativeValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            Console.WriteLine("Array: {0}", string.Join(", ", ArrayFilling(array, random)));
+            Console.WriteLine("Sum not negative value = " + SumNotNegativeValue(array));
+            Console.ReadKey();
+        }
+        static int[] ArrayFilling(int[] array, Random random)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-15, 15);
+            }
+            return array;
+        }
+        static int SumNotNegativeValue(int[] array)
+        {
+            int sum = 0;
+            foreach (int i in array)
+            {
+                if (i >= 0) sum += i;
+            }
+            return sum;
+        }
+    }
+}
