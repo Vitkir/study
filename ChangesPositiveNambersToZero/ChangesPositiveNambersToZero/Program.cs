98fb7949e9902c476cee6de67b8ba5f345aba4e1
Vitkir <kirillcherevko@gmail.com>
2019-08-13 23:55:25 +0400

    Task for verification.

--- ChangesPositiveNambersToZero/ChangesPositiveNambersToZero/Program.cs
+++ ChangesPositiveNambersToZero/ChangesPositiveNambersToZero/Program.cs
@@ -10,7 +10,52 @@ namespace ChangesPositiveNambersToZero
     {
         static void Main(string[] args)
         {
-            int[,,] array = new int[5, 5, 5];
+            int arraySize = 2;
+            int[,,] array = new int[arraySize, arraySize, arraySize];
+            Random random = new Random();
+            int[,,] array1 = ArrayFilling(array, random, arraySize);
+            Print(array1);
+            int[,,] array2 = ChangePositiveValueFromArray(array, arraySize);
+            Print(array2);
+            Console.ReadKey();
+        }
+        static int[,,] ArrayFilling(int[,,] array, Random random, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        array[i, j, k] = random.Next(-50, 50);
+                    }
+                }
+            }
+            return array;
+        }
+
+        static int[,,] ChangePositiveValueFromArray(int[,,] array, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        if (array[i, j, k] > 0) array[i, j, k] = 0;
+                    }
+                }
+            }
+            return array;
+        }
+
+        static void Print(int[,,] array)
+        {
+            Console.WriteLine();
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
