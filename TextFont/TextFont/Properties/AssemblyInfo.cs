9987f37108e0cf1c6525a55829de8e001b1ae02c
Vitkir <kirillcherevko@gmail.com>
2019-08-13 12:29:55 +0400

    Rename project.

--- TextFont/TextFont/Properties/AssemblyInfo.cs
+++ TextFont/TextFont/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TextStyles")]
+[assembly: AssemblyTitle("TextFont")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TextStyles")]
+[assembly: AssemblyProduct("TextFont")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
