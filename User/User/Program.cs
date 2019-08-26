f3f52a94f14caa9e3e5a8a52bd9bc1282a1eb899
Vitkir <kirillcherevko@gmail.com>
2019-08-26 23:41:02 +0400

    Task 2.3

---
+++ User/User/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var users = new List<User>();
+            //users.Add(GetUserFromConsole());
+            var user = GetUserFromConsole();
+            Console.WriteLine(user);
+            Console.ReadKey();
+        }
+        static User GetUserFromConsole()
+        {
+            return new User(GetTextFromConsole(), GetTextFromConsole(), GetTextFromConsole(), GetDateTime());
+        }
+        static string GetTextFromConsole()
+        {
+            return Console.ReadLine();
+        }
+        static DateTime GetDateTime()
+        {
+            if (DateTime.TryParseExact(GetTextFromConsole(), "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            {
+                return date;
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect dates format");
+            }
+        }
+    }
+}
---
+++ User/User/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class User
+    {
+        private DateTime birthday;
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string SurName { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return DateTime.Today.Year - Birthday.Year;
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get => birthday;
+            set
+            {
+                if (DateTime.Today > value)
+                {
+                    birthday = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect date entered");
+                }
+            }
+        }
+
+        public User(string name, string surname, string patr, DateTime date)
+        {
+            Name = name;
+            SurName = surname;
+            Patronymic = patr;
+            Birthday = date;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{SurName} {Name} {Patronymic}" +
+                                  "{0}{1}" +
+                                  "{0}{2}",
+                                  Environment.NewLine, Birthday.ToString("dd.MM.yyyy"), Age.ToString());
+        }
+    }
+}
