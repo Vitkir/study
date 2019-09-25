6272d5f290cdccccecb52f20977fda8ecafb6eb7
Vitkir <kirillcherevko@gmail.com>
2019-09-25 16:22:25 +0400

    Task 5.1

---
+++ ArraySorting/ArraySorting/MyExtensionMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ArraySorting
+{
+	public delegate int Compare<T>(T item1, T item2);
+
+	public static class ArrayExtensionMethod
+	{
+		public static void Sort<T>(this Array array, Compare<T> compare)
+		{
+
+		}
+	}
+}
---
+++ ArraySorting/ArraySorting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArraySorting
+{
+	class Program
+	{
+		static void Main()
+		{
+			Compare<int> compare = CompareInt;
+			int[] vs = new int[0];
+			vs.Sort(compare);
+		}
+		public static int CompareInt(int item1, int item2)
+		{
+			if (item1 > item2)
+				return 1;
+			else if (item2 > item1)
+				return -1;
+			else return 0;
+		}
+	}
+}
