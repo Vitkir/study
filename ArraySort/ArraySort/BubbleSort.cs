d82c13e50a143f87a52ffaaef885d8627713c29c
Vitkir <kirillcherevko@gmail.com>
2019-09-25 16:20:31 +0400

    Code refactoring

---
+++ ArraySort/ArraySort/BubbleSort.cs
@@ -0,0 +1,54 @@
+﻿namespace ArraySort
+{
+	public static class BubbleSort
+	{
+		public static int MaxValue(int[] array)
+		{
+			int maxValue = 0;
+			foreach (int value in array)
+			{
+				if (value > maxValue)
+				{
+					maxValue = value;
+				}
+			}
+			return maxValue;
+		}
+
+		public static int MinValue(int[] array, int MaxValue)
+		{
+			int minValue = MaxValue;
+			foreach (int value in array)
+			{
+				if (value < minValue)
+				{
+					minValue = value;
+				}
+			}
+			return minValue;
+		}
+
+		static void Swap(ref int e1, ref int e2)
+		{
+			var temp = e1;
+			e1 = e2;
+			e2 = temp;
+		}
+
+		public static int[] GetSort(int[] sortArray)
+		{
+			var len = sortArray.Length;
+			for (var j = 1; j < len; j++)
+			{
+				for (var k = 0; k < len - j; k++)
+				{
+					if (sortArray[k] > sortArray[k + 1])
+					{
+						Swap(ref sortArray[k], ref sortArray[k + 1]);
+					}
+				}
+			}
+			return sortArray;
+		}
+	}
+}
--- ArraySort/ArraySort/Program.cs
+++ ArraySort/ArraySort/Program.cs
@@ -5,16 +5,16 @@ namespace ArraySort
     class Program
     {
 
-        public static void Main(string[] args)
+        public static void Main()
         {
             int[] array = new int[10];
             Random random = new Random();
             Console.WriteLine("Unsorted array: {0}", string.Join(", ", FillArray(array, random)));
-            int maxValue = MaxValue(array);
+			int maxValue = BubbleSort.MaxValue(array);
             Console.WriteLine("Max value array: " + maxValue);
-            int minValue = MinValue(array, maxValue);
+            int minValue = BubbleSort.MinValue(array, maxValue);
             Console.WriteLine("Min value array: " + minValue);
-            Console.WriteLine("Sorted array: {0}", string.Join(", ", BubbleSort(array)));
+            Console.WriteLine("Sorted array: {0}", string.Join(", ", BubbleSort.GetSort(array)));
             Console.ReadKey();
         }
 
@@ -27,53 +27,5 @@ namespace ArraySort
             return unsortedArray;
         }
 
-        static int MaxValue(int[] array)
-        {
-            int maxValue = 0;
-            foreach (int value in array)
-            {
-                if (value > maxValue)
-                {
-                    maxValue = value;
-                }
-            }
-            return maxValue;
-        }
-
-        static int MinValue(int[] array, int MaxValue)
-        {
-            int minValue = MaxValue;
-            foreach (int value in array)
-            {
-                if (value < minValue)
-                {
-                    minValue = value;
-                }
-            }
-            return minValue;
-        }
-
-        static void Swap(ref int e1, ref int e2)
-        {
-            var temp = e1;
-            e1 = e2;
-            e2 = temp;
-        }
-
-        static int[] BubbleSort(int[] sortArray)
-        {
-            var len = sortArray.Length;
-            for (var j = 1; j < len; j++)
-            {
-                for (var k = 0; k < len - j; k++)
-                {
-                    if (sortArray[k] > sortArray[k + 1])
-                    {
-                        Swap(ref sortArray[k], ref sortArray[k + 1]);
-                    }
-                }
-            }
-            return sortArray;
-        }
     }
 }
