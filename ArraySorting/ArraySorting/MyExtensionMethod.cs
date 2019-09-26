607c4ed531395c55aec004d0f7cdd8df55ad9f7d
Vitkir <kirillcherevko@gmail.com>
2019-09-26 15:55:18 +0400

    Code refactoring

--- ArraySorting/ArraySorting/MyExtensionMethod.cs
+++ ArraySorting/ArraySorting/MyExtensionMethod.cs
@@ -1,14 +1,30 @@
-﻿using System;
-
-namespace ArraySorting
+﻿namespace ArraySorting
 {
 	public delegate int Compare<T>(T item1, T item2);
 
 	public static class ArrayExtensionMethod
 	{
-		public static void Sort<T>(this Array array, Compare<T> compare)
+		static void StoogeSort<T>(T[] array, int startIndex, int endIndex, Compare<T> compare)
 		{
+			if (compare(array[startIndex], array[endIndex]) == -1)
+			{
+				var temp = array[startIndex];
+				array[startIndex] = array[endIndex];
+				array[endIndex] = temp;
+			}
 
+			if (endIndex - startIndex > 1)
+			{
+				int len = (endIndex - startIndex + 1) / 3;
+				StoogeSort(array, startIndex, endIndex - len, compare);
+				StoogeSort(array, startIndex + len, endIndex, compare);
+				StoogeSort(array, startIndex, endIndex - len, compare);
+			}
+		}
+
+		public static void StoogeSort<T>(this T[] array, Compare<T> compare)
+		{
+			StoogeSort(array, 0, array.Length - 1, compare);
 		}
 	}
 }
--- ArraySorting/ArraySorting/Program.cs
+++ ArraySorting/ArraySorting/Program.cs
@@ -6,17 +6,46 @@ namespace ArraySorting
 	{
 		static void Main()
 		{
-			Compare<int> compare = CompareInt;
-			int[] vs = new int[0];
-			vs.Sort(compare);
+			Compare<string> compare = CompareString;
+			string[] vs = { "There", "are", "many", "big", "and", "small", "libraries", "everywhere", "in", "our", "country" };
+			Print(vs);
+			vs.StoogeSort(compare);
+			PrintString(vs);
+			Console.ReadKey();
 		}
+
 		public static int CompareInt(int item1, int item2)
 		{
-			if (item1 > item2)
+			if (item1 < item2)
 				return 1;
-			else if (item2 > item1)
+			else if (item2 < item1)
 				return -1;
 			else return 0;
+		}
+
+		public static int CompareString(string str1, string str2)
+		{
+			if (str1.Length < str2.Length)
+				return 1;
+			else if (str2.Length < str1.Length)
+				return -1;
+			else return 0;
+		}
+		static void Print(Array array)
+		{
+			foreach (var item in array)
+			{
+				Console.Write(item.ToString() + " ");
+			}
+			Console.WriteLine();
+		}
+		static void PrintString(Array array)
+		{
+			foreach (string item in array)
+			{
+				Console.WriteLine(item.ToString() + " - " + item.Length);
+			}
+			Console.WriteLine();
 		}
 	}
 }
