f7cadffc60fda1b433fcae392f023a3cd386d045
Vitkir <kirillcherevko@gmail.com>
2019-09-27 17:23:07 +0400

    Code refactoring

---
+++ ArrayIntExtension/ArrayIntExtension/ArraySorting.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+
+namespace ArraySorting
+{
+
+	public class ArraySort<T>
+	{
+		public delegate int Compare(T item1, T item2);
+		public delegate void Print(T[] array);
+
+		public static event Print printToConsole;
+
+		private static void StoogeSort(T[] array, int startIndex, int endIndex, Compare compare)
+		{
+			if (compare(array[startIndex], array[endIndex]) == -1)
+			{
+				var temp = array[startIndex];
+				array[startIndex] = array[endIndex];
+				array[endIndex] = temp;
+			}
+
+			if (endIndex - startIndex > 1)
+			{
+				int len = (endIndex - startIndex + 1) / 3;
+				StoogeSort(array, startIndex, endIndex - len, compare);
+				StoogeSort(array, startIndex + len, endIndex, compare);
+				StoogeSort(array, startIndex, endIndex - len, compare);
+			}
+		}
+
+		public static void StoogeSort(T[] array, Compare compare)
+		{
+			StoogeSort(array, 0, array.Length - 1, compare);
+			printToConsole(array);
+		}
+
+		public static void SortInStream(T[] array, Compare compare)
+		{
+			Thread thread = new Thread(() => StoogeSort(array, compare));
+			thread.Start();
+		}
+	}
+}
