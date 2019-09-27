d5b2d44deed7cda0fd490e5213db6f6c7fa0a8e5
Vitkir <kirillcherevko@gmail.com>
2019-09-27 15:08:32 +0400

    Task 5.4

---
+++ ArrayIntExtension/ArrayIntExtension/ArrayIntExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace ArrayIntExtension
+{
+	public static class ArrayIntExtension
+	{
+		public static int SumArrayElements(this int[] array)
+		{
+			int sum = 0;
+			foreach (var item in array)
+			{
+				sum += item;
+			}
+			return sum;
+		}
+	}
+}
---
+++ ArrayIntExtension/ArrayIntExtension/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrayIntExtension
+{
+	class Program
+	{
+		static void Main()
+		{
+			int[] vs = { 2, 5, 6, 8, 1, 3, 6 };
+			int sumItem = vs.SumArrayElements();
+			Console.WriteLine(sumItem.ToString());
+			Console.ReadKey();
+		}
+	}
+}
