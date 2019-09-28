6dc7fdb37950af7a97d1db8d838fc836a4db1917
Vitkir <kirillcherevko@gmail.com>
2019-09-28 15:07:13 +0400

    Code refactorinf

---
+++ ArraySearch/ArraySearch/ArraySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArraySearch
+{
+	static class ArraySearch
+	{
+		public delegate bool IsEquals(int item);
+
+		public static void GetNumbers(int[] array, Action<int> print)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] > 0)
+					print(array[i]);
+			}
+			Console.WriteLine();
+		}
+
+		public static void GetNumbers(int[] array, IsEquals condition, Action<int> print)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (condition(array[i]))
+					print(array[i]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ ArraySearch/ArraySearch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ArraySearch
+{
+	class Program
+	{
+		static void Main()
+		{
+			Action<int> print = PrintToConsole;
+
+			ArraySearch.IsEquals isEquals1 = IsPositive;
+
+			ArraySearch.IsEquals isEquals2 = delegate (int item)
+			{
+				if (item > 0)
+					return true;
+				else return false;
+			};
+
+			ArraySearch.IsEquals isEquals3 = item => item > 0;
+
+			Stopwatch stopwatch = new Stopwatch();
+
+			int[] vs = { 2, -5, 0, 2, 6, 8, -5, -6, -9, 56, -78, 4 };
+
+			stopwatch.Start();
+
+			var result = from item in vs
+						 where item < 0
+						 select item;
+			PrintToConsole(result);
+
+			ArraySearch.GetNumbers(vs, print);
+			ArraySearch.GetNumbers(vs, isEquals1, print);
+			ArraySearch.GetNumbers(vs, isEquals2, print);
+			ArraySearch.GetNumbers(vs, isEquals3, print);
+
+			stopwatch.Stop();
+			var time = stopwatch.ElapsedMilliseconds;
+			Console.WriteLine(time.ToString());
+
+			Console.ReadKey();
+		}
+
+		static bool IsPositive(int item)
+		{
+			if (item > 0)
+				return true;
+			else return false;
+		}
+
+		static void PrintToConsole(int item)
+		{
+			Console.Write(item.ToString() + " ");
+		}
+
+		static void PrintToConsole(IEnumerable<int> result)
+		{
+			foreach (var item in result)
+			{
+				Console.Write(item.ToString() + " ");
+			}
+		}
+	}
+}
