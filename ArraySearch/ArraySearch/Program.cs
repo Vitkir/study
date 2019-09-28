7b9d2d230623db90bea30e4704bcccdd0369c019
Vitkir <kirillcherevko@gmail.com>
2019-09-28 15:32:06 +0400

    Code refactoring

--- ArraySearch/ArraySearch/Program.cs
+++ ArraySearch/ArraySearch/Program.cs
@@ -24,24 +24,30 @@ namespace ArraySearch
 
 			Stopwatch stopwatch = new Stopwatch();
 
-			int[] vs = { 2, -5, 0, 2, 6, 8, -5, -6, -9, 56, -78, 4 };
-
+			int[] vs = {66, 80, 11, -33, -99, -71, 25, -65, 36,
+				-94, 17, 4, 10, 6, 86, 62, 65, 49, -13, 89, -34, 90,
+				91, -6, 37, 18, -8, -80, -92, 29, -39, 50, 15, 3, 46,
+				84, -12, 81, 95, -89, 83, -48, -85, -1, 16, 24, -56, 8,
+				64, -20, -26, -51, 14, 27, 68, -37, 51, -2, 41, -77, -17,
+				44, -55, -31, 40, -68, -63, 97, -100, -96, 99};
+			int count = 0;
 			stopwatch.Start();
-
-			var result = from item in vs
-						 where item < 0
-						 select item;
-			PrintToConsole(result);
-
-			ArraySearch.GetNumbers(vs, print);
-			ArraySearch.GetNumbers(vs, isEquals1, print);
-			ArraySearch.GetNumbers(vs, isEquals2, print);
-			ArraySearch.GetNumbers(vs, isEquals3, print);
-
+			while (count < 10)
+			{
+				var result = from item in vs
+							 where item < 0
+							 select item;
+				PrintToConsole(result);
+				ArraySearch.GetNumbers(vs, print);
+				ArraySearch.GetNumbers(vs, isEquals1, print);
+				ArraySearch.GetNumbers(vs, isEquals2, print);
+				ArraySearch.GetNumbers(vs, isEquals3, print);
+				count++;
+			}
 			stopwatch.Stop();
 			var time = stopwatch.ElapsedMilliseconds;
+			Console.Clear();
 			Console.WriteLine(time.ToString());
-
 			Console.ReadKey();
 		}
 
