61a23869772a46b76c549b78fea89ec78949ffd4
Vitkir <kirillcherevko@gmail.com>
2019-10-03 15:39:53 +0400

    New task 4.1

---
+++ AverageWordLength/AverageWordLength/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AverageWordLength
+{
+	class Program
+	{
+		static void Main()
+		{
+			string text = Console.ReadLine();
+			Console.WriteLine(WordLengthCounter.GetAverageWordLength(text).ToString());
+			Console.ReadKey();
+		}
+	}
+}
