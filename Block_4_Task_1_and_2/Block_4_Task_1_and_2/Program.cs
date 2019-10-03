a5c99cc9a9c48db8b22721afeaa33c00a1d7f442
Vitkir <kirillcherevko@gmail.com>
2019-10-03 19:32:41 +0400

    Code refactoring

---
+++ Block_4_Task_1_and_2/Block_4_Task_1_and_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Block_4_Task_1_and_2
+{
+	class Program
+	{
+		static void Main()
+		{
+			string text = Console.ReadLine();
+			string pattern = Console.ReadLine();
+			Console.WriteLine(WorkWithString.DoublingLetter(text, pattern).ToString());
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Block_4_Task_1_and_2/Block_4_Task_1_and_2/WorkWithString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Block_4_Task_1_and_2
+{
+	class WorkWithString
+	{
+		public static double GetAverageWordLength(string text)
+		{
+			var validLetter = text.ToCharArray().Where(letter => char.IsLetter(letter) || char.IsSeparator(letter)).ToArray();
+			string validString = new string(validLetter);
+
+			string[] words = validString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			int averageWordLength = (int)words.Average(word => word.Length);
+			return averageWordLength;
+		}
+
+		public static StringBuilder DoublingLetter(string t1, string t2)
+		{
+			char[] text = t1.ToCharArray();
+			char[] pattern = t2.ToCharArray();
+			StringBuilder result = new StringBuilder();
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (pattern.Contains(text[i]))
+					result.Append(text[i], 2);
+				else result.Append(text[i]);
+			}
+			return result;
+		}
+	}
+}
