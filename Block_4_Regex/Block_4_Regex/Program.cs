59310bd960187bce9415480c4bd66eb1dd377343
Vitkir <kirillcherevko@gmail.com>
2019-10-04 16:16:59 +0400

    New tasks regex

---
+++ Block_4_Regex/Block_4_Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Block_4_Regex
+{
+	class Program
+	{
+		static void Main()
+		{
+			string text = Console.ReadLine();
+			var emails = StringSearch.GetEmail(text);
+			foreach (var item in emails)
+			{
+				Console.WriteLine(item.ToString());
+			}
+			//Console.WriteLine(StringSearch.GetEmail(text));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Block_4_Regex/Block_4_Regex/StringSearch.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Block_4_Regex
+{
+	static class StringSearch
+	{
+		static Regex validDate = new Regex("(0[1-9]|[1-2][0-9]|3[0,1])-(0[1-9]|1[1,2])-(000[1-9]|[0-9])");
+		static Regex htmlTag = new Regex(@"<[^>]+>");
+		static Regex email = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+		public static bool IsValidDate(string date)
+		{
+			return validDate.IsMatch(date);
+		}
+
+		public static string DeleteHTMLTags(string text)
+		{
+			return Regex.Replace(text, htmlTag.ToString(), "_").ToString();
+		}
+
+		public static MatchCollection GetEmail(string text)
+		{
+			MatchCollection emails = Regex.(text, email.ToString());
+			return emails;
+		}
+	}
+}
