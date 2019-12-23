d58aef19ccc03258675b9f1fee0f113ca6dababd
Vitkir <kirillcherevko@gmail.com>
2019-12-23 15:51:37 +0400

    Task 7.2 complete

---
+++ Block7/Block7/JavaScript7_2.js
@@ -0,0 +1,26 @@
+﻿calculate.onclick = function () {
+	let input = document.getElementById('inputExpression').value + "",
+		pattern = /([/+-/*/]?)([ ]?)(([0-9]+\.[0-9]+)|[0-9]+)/g,
+		exp = input.match(pattern),
+		result = 0;
+	for (var i = 0; i < exp.length; i++) {
+		switch (exp[i][0]) {
+			case "+":
+				result = result + +exp[i].substring(1);
+				break;
+			case "-":
+				result -= +exp[i].substring(1);
+				break;
+			case "*":
+				result *= +exp[i].substring(1);
+				break;
+			case "/":
+				result /= +exp[i].substring(1);
+				break;
+			default:
+				result += +exp[i];
+				break;
+		}
+	}
+	document.getElementById('expressionResult').innerHTML = "result: " + result.toFixed(2);
+}
