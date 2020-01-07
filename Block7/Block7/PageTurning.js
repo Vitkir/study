d89be3dcf42aff4de433e2ae59e98449e88bf7dc
Vitkir <kirillcherevko@gmail.com>
2020-01-08 03:00:43 +0400

    Append html elements

--- Block7/Block7/PageTurning.js
+++ Block7/Block7/PageTurning.js
@@ -1,1 +1,26 @@
-﻿
+﻿function appendElements() {
+	let container = document.createElement('div'),
+		previousPage = document.createElement('a'),
+		timerButton = document.createElement('button'),
+		timer = document.createElement('span');
+
+	previousPage.setAttribute('href', document.referrer);
+	previousPage.innerHTML = "Go to previous page";
+	previousPage.setAttribute('class', 'button');
+	timer.setAttribute('id', 'timer');
+
+	container.appendChild(previousPage);
+	container.appendChild(timerButton);
+	container.appendChild(timer);
+
+	document.body.appendChild(container);
+
+	setInterval(function () {
+		let timer = 5000;
+
+		if (timer < 0) {
+			window.location.href = '../Task2/Task2.html';
+			document.getElementById("timer").innerHTML = timer;
+		}
+	}, timer);
+}
