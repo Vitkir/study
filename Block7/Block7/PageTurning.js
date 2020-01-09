3e782b345451b7fdf49ccd7b887452b5b1de0946
Vitkir <kirillcherevko@gmail.com>
2020-01-09 15:27:37 +0400

    Add auto move to next pages

--- Block7/Block7/PageTurning.js
+++ Block7/Block7/PageTurning.js
@@ -1,26 +1,57 @@
-﻿function appendElements() {
-	let container = document.createElement('div'),
-		previousPage = document.createElement('a'),
-		timerButton = document.createElement('button'),
-		timer = document.createElement('span');
+﻿let timer = false;
 
-	previousPage.setAttribute('href', document.referrer);
+function appendElements() {
+	let container = document.createElement('div'),
+		previousPage = document.createElement('button'),
+		timerButton = document.createElement('button'),
+		icon = document.createElement('i');
+
+	previousPage.addEventListener('click', back);
 	previousPage.innerHTML = "Go to previous page";
-	previousPage.setAttribute('class', 'button');
-	timer.setAttribute('id', 'timer');
+	previousPage.setAttribute('class', "back");
+
+	icon.classList.add("fas");
+	icon.classList.add("fa-stop-circle");
+
+	timerButton.setAttribute('id', "timerBtn");
+	timerButton.addEventListener('click', timerControl);
+	timerButton.addEventListener('click', timerButtonStyle)
+	timerButton.appendChild(icon);
 
 	container.appendChild(previousPage);
 	container.appendChild(timerButton);
-	container.appendChild(timer);
 
 	document.body.appendChild(container);
 
-	setInterval(function () {
-		let timer = 5000;
+	timerControl();
+}
 
-		if (timer < 0) {
-			window.location.href = '../Task2/Task2.html';
-			document.getElementById("timer").innerHTML = timer;
-		}
-	}, timer);
+function timerControl() {
+	let ref = document.getElementById('path');
+
+	if (timer === false) {
+		timer = setInterval(function () {
+			window.location.href = ref.href;
+		}, 5000);
+	} else {
+		clearInterval(timer);
+		timer = false;
+	}
+}
+
+function timerButtonStyle(event) {
+	let timerbutton = event.target,
+		icon = document.getElementsByTagName('i');
+
+	if (icon[0].className === "fas fa-stop-circle") {
+		timerbutton.classList.remove("fa-stop-circle");
+		timerbutton.classList.add("fa-play-circle");
+	} else {
+		timerbutton.classList.remove("fa-play-circle");
+		timerbutton.classList.add("fa-stop-circle");
+	}
+}
+
+function back() {
+	window.history.back()
 }
